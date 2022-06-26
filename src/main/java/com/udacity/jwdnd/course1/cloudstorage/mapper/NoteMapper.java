2d909a859b13820e5227b1e12597cd420d6c8b76
Shimin LIANG <shiminliang@ShimindeMacBook-Pro.local>
2022-06-25 17:17:01 -0700

    Add Note model, NoteForm and NoteMapper

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,27 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface NoteMapper {
+    @Select("SELECT * FROM NOTES")
+    List<Note> getAllNote();
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+    List<Note> getNotesByUser(Integer userId);
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
+    Note getNote(Integer noteId);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int insert(Note note);
+
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
+    void delete(Integer noteId);
+
+    @Update("UPDATE NOTES SET notetitle = #{noteTitle}, notedescription = #{noteDescription} WHERE noteid = #{noteId}")
+    void update(Integer noteId, String noteTitle, String noteDescription);
+}
